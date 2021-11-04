2403409f8ed52718b0893eb95c96bfa464ee81c3
João Falcão <jvfsl@ic.ufal.br>
2021-11-04 16:54:57 -0300

    initial commit: project config + google test

---
+++ src/test/java/br/jao/TesteGoogle.java
@@ -0,0 +1,29 @@
+package br.jao;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TesteGoogle {
+    
+    @Test
+    public void teste() {
+        WebDriver driver = new FirefoxDriver();
+        // WebDriver driver = new ChromeDriver();
+        
+        // Posição e Tamanho do Broser
+        // driver.manage().window().setPosition(new Point(0, 0));
+        // driver.manage().window().setSize(new Dimension(1200, 660));
+        // driver.manage().window().maximize();
+        
+        driver.get("https://google.com");
+        Assert.assertEquals("Google", driver.getTitle());
+
+        driver.quit();
+    }
+
+}
